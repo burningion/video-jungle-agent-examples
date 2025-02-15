789c5e1b53d98428d5107cf82bad3f8bab41dfd8
Kirk Kaiser <kirk@zothcorp.com>
2025-02-15 11:08:31 -0500

    about to add yt-dlp

---
+++ marketing_video_editor/src/marketing_video_editor/tools/videojungle_download.py
@@ -0,0 +1,34 @@
+from typing import Any, Optional, Type, List
+from pydantic import BaseModel, ConfigDict
+from .videojungle_basetool import VideoJungleApiBaseTool
+from datetime import datetime
+
+class VideoJungleDownloadTool(VideoJungleApiBaseTool):
+    """Tool for uploading video files and assets to Video Jungle"""
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True, validate_assignment=True, frozen=False
+    )
+    name: str = "Video Jungle Video Downloader"
+    description: str = (
+        "A tool to download and analyze videos on Video Jungle."
+    )
+    args_schema: Type[BaseModel] = VideoJungleApiBaseTool.VideoUploadSchema
+    def _run(
+        self,
+        name: str,
+        filename: str,
+        upload_method: Optional[str] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Create and uplload a video file from a local file path or URL.
+        Upload method must be 'url', and filename must contain the URL in order to download from a url.
+        """
+        try:
+            if upload_method != 'url':
+                self.client.video_files.create(name=name, filename=filename)
+            elif upload_method == 'url':
+                self.client.video_files.create(name=name, filename=filename, upload_method=upload_method)
+            
+        except Exception as e:
+            return f"An error occurred during video edit creation: {str(e)}"
